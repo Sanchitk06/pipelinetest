20b0485378db5c7108a9cfadaecc3dcd7abb07a8
Sanchitk06 <sanchitshinde@gmail.com>
2025-01-26 15:57:16 +0530

    Initial commit for Maven pipeline setup

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testOutput() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
